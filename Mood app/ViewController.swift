6126482ac93bdeedabc1ad8ff3e45a32030fffa2
Sam Ellenrieder <sam.ell@web.de>
2017-10-09 15:29:48 +0100

    Initial Commit

---
+++ Mood app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mood app
+//
+//  Created by Sam Ellenrieder on 09/10/2017.
+//  Copyright © 2017 Sam Ellenrieder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
